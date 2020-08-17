fe4bd74fd5a5d531e6210c53283d7510dae86fa7
Prathyush <hi@prabros.com>
2020-08-17 17:44:10 +0530

    Working on exercise 2.1.2

---
+++ exercises/exercise-2.1.1.scm
@@ -0,0 +1,7 @@
+;; <list-of-numbers>
+;; => (<number> . <list-of-numbers>)
+;; => (-7 . <list-of-numbers>)
+;; => (-7 . (<number> . <list-of-numbers>))
+;; => (-7 . (3 . <list-of-numbers>))
+;; => (-7 . (3 . (<number> . <list-of-numbers>)))
+;; => (-7 . (3 . (14 . ())))
