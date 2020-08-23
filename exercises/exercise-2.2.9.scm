9d9934749e824677e8b08303179bf14f1d18447c
Prathyush <hi@prabros.com>
2020-08-24 00:24:01 +0530

    Update timetable with progress on Exercise 2.2.9

--- exercises/exercise-2.2.9.scm
+++ exercises/exercise-2.2.9.scm
@@ -20,17 +20,42 @@
 
 (define (build-selector s slst errvalue)
     (if (null? slst) errvalue
-          (if (equal? s (car slst))'(car lst)
+          (if (equal? s (car slst)) '(lst)
                  (if (equal? (build-selector s (cdr slst) errvalue) errvalue)
                      errvalue
-                     (list 'cdr (build-selector s (cdr slst) errvalue))))))
+                     (cons 'cdr (build-selector s (cdr slst) errvalue))))))
+
+(define (process-exp s exp errvalue)
+  (if (symbol? exp)
+      (if (equal? s exp) 'lst errvalue)
+       (if (null? exp) errvalue
+           (if (equal? (process-exp s (car exp) errvalue) errvalue)
+               (if (equal? (car&cdr-helper s (cdr exp) errvalue) errvalue)
+                   errvalue
+                   (list 'cdr (car&cdr-helper s (cdr exp) errvalue)))
+               (list 'car (process-exp s (car exp) errvalue))))))
+
+;; s-exp: symbol | s-list
+;; s-list: ({<s-exp>}*)
+(define (car&cdr-helper s slst errvalue)
+  (if (null? slst) errvalue
+  (if (equal? (process-exp s (car slst) errvalue) errvalue)
+      (if (equal? (car&cdr-helper s (cdr slst) errvalue) errvalue)
+          errvalue
+          (list 'cdr (car&cdr-helper s (cdr slst) errvalue)))
+      (process-exp s (car slst) errvalue))))
 
 (define (car&cdr s slst errvalue)
-  (if (equal? (build-selector s slst errvalue) errvalue) errvalue
-      (list 'lambda `(lst) (build-selector s slst errvalue))))
+  (if (equal? (car&cdr-helper s slst errvalue) errvalue)
+      errvalue
+      (list 'lambda '(lst) (list 'car (car&cdr-helper s slst errvalue)))))
+      
 
 (equal? (car&cdr 'a '(a b c) 'fail) '(lambda (lst) (car lst)))
+(equal? (car&cdr 'a '() 'fail) 'fail)
+(equal? (car&cdr 'a '(() () () a) 'fail) '(lambda (lst) (car (cdr (cdr (cdr lst))))))
 (equal? (car&cdr 'c '(a b c) 'fail) '(lambda (lst) (car (cdr (cdr lst)))))
+(equal? (car&cdr 'apple '((apple)) 'fail) '(lambda (lst) (car (car lst))))
 (equal? (car&cdr 'dog '(cat lion (fish dog) pig) 'fail) '(lambda (lst)
                                                            (car (cdr (car (cdr (cdr lst)))))))
 (equal? (car&cdr 'a '(b c) 'fail) 'fail)
